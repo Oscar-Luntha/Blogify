7516d8fb2cd719a029ceb3270e8e99afee6f0399
Oscar Luntha <lunthaoscar@gmail.com>
2025-06-11 15:24:05 +0200

    Fix route error in the authors page

--- app/author/page.tsx
+++ app/author/page.tsx
@@ -35,7 +35,7 @@ export default async function AuthorPage() {
           <p className="text-slate-400">Here's what's happening with your content.</p>
         </div>
 
-        <Link href="/create-post" className="flex items-center gap-2 bg-yellow-300 text-slate-950 px-4 py-2 rounded-lg hover:bg-yellow-400 transition">
+        <Link href="/author/create-post" className="flex items-center gap-2 bg-yellow-300 text-slate-950 px-4 py-2 rounded-lg hover:bg-yellow-400 transition">
           <PlusCircle size={20} /> Create New Post
         </Link>
       </div>
